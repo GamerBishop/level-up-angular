917ba0e8e1c676cb5492ead7c238e0cc5c9287f7
Gamerbishop <hugo.levk@gmail.com>
2025-01-14 15:20:33 +0100

    Déplacement des pages Ajout du composant FormControlError

---
+++ src/app/components/form-control-error/form-control-error.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control-error',
+  imports: [],
+  templateUrl: './form-control-error.component.html',
+  styleUrl: './form-control-error.component.css'
+})
+export class FormControlErrorComponent {
+
+}
